a549b188b0718af1a53126942fbaf9b1251388c1
Ankit <ankitphondani52@gmail.com>
2020-07-20 18:55:30 +0530

    Updates

---
+++ precache-manifest.9c664d755708c8e48c1835f6ea1b22bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46f25e18dfcc96e00ff45b473cd89348",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bfa94a49c0160dbbb62",
+    "url": "/static/css/main.28b22cca.chunk.css"
+  },
+  {
+    "revision": "67a686dbfc7893aff7bb",
+    "url": "/static/js/2.8ee0e38c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8ee0e38c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bfa94a49c0160dbbb62",
+    "url": "/static/js/main.7dbd5185.chunk.js"
+  },
+  {
+    "revision": "31b66ed331bd76c22c38",
+    "url": "/static/js/runtime-main.389e0a99.js"
+  }
+]);
